83d127950189bae57f2d33b050a5893fc9d51fbe
danielsanchez2020 <115204457+danielsanchez2020@users.noreply.github.com>
2022-11-11 10:53:53 -0400

    Add files via upload

---
+++ HighschoolTrainee.java
@@ -0,0 +1,16 @@
+public class HighschoolTrainee {
+    public static void main(String[] args) {
+        int StudentAge = 35;
+        double Prom = 9.5;
+        char FirstInitial = 'D';
+        char LastInitial = 'L';
+        boolean Attendace = true;
+
+
+        System.out.println("The student age is: "+ StudentAge);
+        System.out.println("The prom is: "+ Prom);
+        System.out.println("THe first initial is: "+ FirstInitial);
+        System.out.println("The last initial is: "+ LastInitial);
+        System.out.println("The attendance is: "+ Attendace);
+    }
+}
